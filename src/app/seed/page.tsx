bce2f4d7d67bd5a14149242e5a47c81aeb8117e2
Fernando Costa <costa.fernando@jovensgenios.com>
2025-03-22 09:02:07 -0300

    ♻️ Passando `seedDb` server action para o arquivo de actions.ts.

--- src/app/seed/page.tsx
+++ src/app/seed/page.tsx
@@ -1,35 +1,6 @@
-import { mockFolders, mockFiles } from "~/lib/mock-data";
-import { db } from "~/server/db";
-import { files, folders } from "~/server/db/schema";
+import { seedDb } from "~/server/actions";
 
 export default function SeedPage() {
-  async function seedDb() {
-    "use server";
-
-    const insertedFiles = await db.insert(files).values(
-      mockFiles.map((file, index) => ({
-        id: index + 1,
-        name: file.name,
-        type: file.type,
-        folderId: (index % 3) + 1,
-        size: parseInt(file.size),
-        ownerId: "1",
-      })),
-    );
-
-    console.log("Inserted files:", insertedFiles);
-
-    const insertedFolders = await db.insert(folders).values(
-      mockFolders.map((folder, index) => ({
-        id: index + 1,
-        name: folder.name,
-        parentId: folder.parent ? parseInt(folder.parent) : null,
-        ownerId: "1",
-      })),
-    );
-    console.log("Inserted folders:", insertedFolders);
-  }
-
   return (
     <div className="min-h-screen bg-gray-900 p-8 text-gray-100">
       <div className="mx-auto max-w-6xl">
--- src/server/actions.ts
+++ src/server/actions.ts
@@ -1,11 +1,35 @@
 "use server";
-import { auth } from "~/lib/auth";
 
-export const signIn = async (email: string, password: string) => {
-  await auth.api.signInEmail({
-    body: {
-      email: email,
-      password: password,
-    },
-  });
-};
+import { db } from "~/server/db";
+import {
+  files as filesSchema,
+  folders as foldersSchema,
+} from "~/server/db/schema";
+import { mockFolders, mockFiles } from "~/lib/mock-data";
+
+export async function seedDb() {
+  "use server";
+
+  const insertedFiles = await db.insert(filesSchema).values(
+    mockFiles.map((file, index) => ({
+      id: index + 1,
+      name: file.name,
+      type: file.type,
+      folderId: (index % 3) + 1,
+      size: parseInt(file.size),
+      ownerId: "1",
+    })),
+  );
+
+  console.log("Inserted files:", insertedFiles);
+
+  const insertedFolders = await db.insert(foldersSchema).values(
+    mockFolders.map((folder, index) => ({
+      id: index + 1,
+      name: folder.name,
+      parentId: folder.parent ? parseInt(folder.parent) : null,
+      ownerId: "1",
+    })),
+  );
+  console.log("Inserted folders:", insertedFolders);
+}
