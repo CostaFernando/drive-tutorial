032742eb42153a711f10309e44d6701c32a56ac8
Fernando Costa <costa.fernando@jovensgenios.com>
2025-03-22 14:09:30 -0300

    ✨ Rota raiz redireciona para o Drive.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import { Button } from "~/components/ui/button";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function HomePage() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      <p className="mt-4 text-lg">This is a simple authentication demo.</p>
-      <Button className="mt-8">
-        <Link href="/sign_in">Sign In</Link>
-      </Button>
-    </div>
-  );
+  redirect("/my_drive/1");
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -12,5 +12,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/my_drive/:path*"],
+  matcher: ["/", "/my_drive/:path*"],
 };
