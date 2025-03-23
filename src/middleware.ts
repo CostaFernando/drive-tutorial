6ed15915e92fb391140eec3c389bbec930ca549b
Fernando Costa <costa.fernando@jovensgenios.com>
2025-03-22 22:33:23 -0300

    🚧 Alteração do middleware.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
+  const sessionCookie = getSessionCookie(request, {
+    // Optionally pass config if cookie name, prefix or useSecureCookies option is customized in auth config.
+    cookieName: "session_token",
+    cookiePrefix: "better-auth",
+    useSecureCookies: true,
+  });
 
   if (!sessionCookie) {
     console.log(
